6ef40357051240d1f8ef6a7c8fdf1e397da2d476
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-03-28 22:32:15 +0300

    updated js file

--- index.js
+++ index.js
@@ -5,3 +5,22 @@
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+function calculateTax (amount) {
+    return amount * 0.10
+}
+function convertToUpperCase (text) {
+    return text.toUpperCase() 
+}
+function findMaximum (num1, num2) {
+    return num1 > num2 ? num1 : num2
+} 
+function isPalindrome (word) {
+    const normalized = word.toLowerCase()
+    const reversed = normalized.split ('').reverse().join('')
+    return normalized === reversed
+}
+function calculateDiscountedPrice (originalPrice, discountedpercentage) {
+    const discountAmount = originalPrice * (discountedpercentage /100)
+    return originalPrice - discountAmount
+} 
